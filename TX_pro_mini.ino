6cf42be70a6b6e6127185e7571df11929a6e7399
Георгий Егоров <georgmix300@mail.ru>
2021-12-10 00:42:44 +0400

    срань v1.0

--- TX_pro_mini.ino
+++ TX_pro_mini.ino
@@ -3,7 +3,6 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <GyverPower.h>
-//#include "TemperatureSensor.h"
 #include "LCD5110.h"
 #include "fonts.h"
 
@@ -14,84 +13,77 @@
 #define NRF2401_POWER       A0        // пин питания nrf24L01
 #define NRF24_CE_PIN        10
 #define NRF24_CSN_PIN       9 
-
 #define BAT_CHARGED_ADC     824       // значeние ацп, при котором напряжение на акб 4.2 вольт (заряжен)
 #define BAT_DISCHARGED_ADC  634       // значeние ацп, при котором напряжение на акб 3.0 вольт (разряжен)
-
-#define BUF_SIZE            13
-
-#define BATERY_POSX         0
+#define NRF24_VOLTAGE_DELAY 1500      // задержка млс для ожидания установки напряжения на понижайке
+#define BUF_SIZE            13        // длинна строки температуры
+#define SLEEP_TIME          60000     // время сна в миллисекундах
+// настройка изображения аккумулятора
+#define BATERY_POSX         84
 #define BATERY_POSY         0
 #define BATERY_HEIGHT       8
 #define BATERY_WIDTH        14
+// настройка LCD
+#define CLK   4
+#define DIN   5
+#define DC    6
+#define RST   8
 
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-RF24 radio(NRF24_CE_PIN, NRF24_CSN_PIN);    // nrf24l01
-LCD5110 myGLCD(4, 5, 6, 8);           // CLK, DIN, DC, RST
-
-const uint32_t sleep_time = 60000;    // время сна в миллисекундах
-uint32_t t_timer_on_RX = 0;
-float tempReload = -100.00;
-
+RF24 radio(NRF24_CE_PIN, NRF24_CSN_PIN);
+LCD5110 myGLCD(CLK, DIN, DC, RST); 
 uint16_t bat_adc = 0;                 // значение с ацп 0...1023 напряжение с акб
 
-uint8_t iStep = 0;                    // шаг перемещения линии зарядки
-bool no_sleeping_flag = false;        // флаг разрешения ухода в сон: "1" запрет, "0" разрешено
-
-bool RF24_inited = true;              // флаг инициализации
-
-extern uint8_t BigNumbers[];
 extern const uint8_t LCDFont[];
 extern const uint8_t Batery[];
+extern const uint8_t Batery75[];
+extern const uint8_t Batery50[];
+extern const uint8_t Batery25[];
 extern const uint8_t LowBatery[];
 extern const uint8_t ChargeBatery[];
-//extern uint8_t DotMatrix_M_Slash[4184];
 
-void isr_charge_up();
-void isr_charge_down();
-void make_temp_string(char *arrayTemp, float tempC);
+void isr_charge();
+void make_temp_string(char *arrayTemp, short tempC);
+void draw_temperature(short tempC);
 void draw_batery();
+bool is_charging();
+void(*resetFunc) (void) = 0; // перезагрузка
+void go_asleep();
+void batery_report();
 
 void setup() {
-  
   Serial.begin(115200);
   
   pinMode(BAT_ADC_PIN, INPUT);
   pinMode(BAT_STDBY_PIN, INPUT);
   pinMode(NRF2401_POWER, OUTPUT);
 
+  digitalWrite(NRF2401_POWER, HIGH);
+  delay(NRF24_VOLTAGE_DELAY);
+  if (radio.begin()) {
+    Serial.println("radio init ok"); // Инициализация модуля NRF24L01
+    radio.setChannel(0x6f);
+    radio.setDataRate(RF24_1MBPS);
+    radio.setPALevel(RF24_PA_HIGH);
+    radio.openWritingPipe(0x7878787878LL);
+  }
+  else {
+    Serial.println("radio init error"); 
+    delay(200);
+    resetFunc();
+  }
+
   myGLCD.InitLCD(70);          //запуск LCD контраст 65
   myGLCD.setFont(LCDFont);
-//  myGLCD.setTextSize(1);  // установка размера шрифта
-  
-//  display.begin();
-//  display.setRotation(2);
-//  display.setFont(&FreeMonoBoldOblique18pt7b);
-//  display.getPixel(0, 0, 0);
-
-  digitalWrite(NRF2401_POWER, HIGH);
-  delay(3000);
   
   sensors.begin();
-  
-  if (!radio.begin()) 
-    Serial.println("radio init error"); // Инициализация модуля NRF24L01
-  else 
-    Serial.println("radio init ok");
-
-  radio.setChannel(0x6f);
-  radio.setDataRate(RF24_1MBPS);
-  radio.setPALevel(RF24_PA_HIGH);
-  radio.openWritingPipe(0x7878787878LL);
 
   //детектор подключения ЗУ с внутренней подтяжкой
   pinMode(BAT_CHRG_PIN, INPUT_PULLUP);
   // аппаратное прерывание: подключение зарядки
-  // подключаем прерывание на пин D2 (Arduino NANO), при подключении
-  attachInterrupt(0, isr_charge_up, FALLING);
-  // подключаем прерывание на пин D2 (Arduino NANO), при отключении
-  attachInterrupt(0, isr_charge_down, RISING);
+  // подключаем прерывание на пин D2 (Arduino NANO)
+  attachInterrupt(0, isr_charge, CHANGE);
   // глубокий сон
   power.setSleepMode(POWERDOWN_SLEEP);
 }
@@ -99,114 +91,51 @@ void setup() {
 void loop() {
   bat_adc = analogRead(BAT_ADC_PIN);
 
-  if (bat_adc >= BAT_CHARGED_ADC) {
-    bat_adc = BAT_CHARGED_ADC;
-  } else if (bat_adc <= BAT_DISCHARGED_ADC) {
-    bat_adc = BAT_DISCHARGED_ADC;
-  }
+  sensors.requestTemperatures();
+  short tempC = sensors.getTempCByIndex(0);
+  radio.write(&tempC, sizeof(tempC));
 
-  sensors.requestTemperatures();          // Send the command to get temperatures
-  float tempC = sensors.getTempCByIndex(0);
-  radio.write(&tempC, sizeof(tempC));     // Передаём данные по радиоканалу
-  //radio.powerDown();
-
-  char arrayTemp[BUF_SIZE] = {0};
-  make_temp_string(arrayTemp, tempC);
-//  myGLCD.clrScr();
-//  myGLCD.printNumI(tempC, CENTER, 13);
-  myGLCD.print(arrayTemp, CENTER, BUF_SIZE);
+  draw_temperature(tempC);
   draw_batery();
+
+  batery_report();
   
-//  display.clearDisplay();
-//  display.fillRect(65, 1, 19, 7, 1);
-//  display.fillRect(66, 2, 17, 5, 0);
-//  display.fillRect(64, 3, 1, 3, 1);
+  go_asleep();
+}
 
-  /*
-   * надо создать буфер, в который поместить значок уровня заряда батареи
-   * потом отправлять этот буфер в дисплей.
-   */
+// обработчик аппаратного прерывания
+void isr_charge() { 
+  power.wakeUp();
+  if (is_charging())
+    Serial.println("Sensor charging!");
+  else
+    Serial.println("Sensor stop charging!");
+}
 
-  Serial.print("bat adc value:  ");
-  Serial.println(bat_adc);
-//
-//  bat_adc = map(bat_adc, BAT_DISCHARGED_ADC, BAT_CHARGED_ADC, 0, 15);
-//  Serial.print("bat_adc: ");
-//  Serial.println(bat_adc);
-//
-//  if (analogRead(BAT_CHRG_PIN) < 500 || analogRead(BAT_STDBY_PIN) < 500) {
-//    Serial.println("Зу подключено ");
-//    //значок постоянный ток на дисплее
-
-//    no_sleeping_flag = 1;
-//    display.writeLine(48, 3, 61, 3, 1);
-//    display.writeLine(48, 5, 51, 5, 1);
-//    display.writeLine(53, 5, 56, 5, 1);
-//    display.writeLine(58, 5, 61, 5, 1);
-//
-//    if (analogRead(BAT_STDBY_PIN) < 500) {
-//      Serial.println("Зарядка АКБ ");
-//      for (uint8_t i = 0; i <= iStep; i++) {
-//        display.writeLine(82 - i, 2, 82 - i, 6, 1);
-//      }
-//      iStep ++;
-//      if (iStep >= 15) iStep = 0;
-//    } else {
-//      Serial.println("Заряжено");
-//      display.fillRect(65, 1, 19, 7, 1);
-//      display.fillRect(68, 4, 13, 1, 0);
-//    }
-//  } else {
-//    Serial.println("Разряжается");
-//    no_sleeping_flag = 0;
-//    for (uint8_t i = 0; i <= bat_adc; i++) {
-//      display.writeLine(82 - i, 2, 82 - i, 6, 1);
-//    }
-//  }
-//
-//  if ((int)tempReload != (int)tempC) {
-//    for (int i = 0; i <= BUF_SIZE; i++) {
-//      if (i != BUF_SIZE) {
-//        display.drawChar( (i * 17), 38, arrayTemp[i]);
-//      } else {
-//        display.drawChar( 5 + (i * 18), 38, arrayTemp[i]);
-//        display.drawChar( (i * 18), 18, '.');
-//      }
-//    }
-//    display.display();
-//  }
-
+void go_asleep() {
   // спим 1 минуту, но можем проснуться по прерыванию
-  uint32_t mls_delay = 60000;
+  uint16_t mls_delay = SLEEP_TIME;
   Serial.print("Sleep mls: ");
   Serial.println(mls_delay);
-  delay(200);
+  delay(10);
   power.sleepDelay(mls_delay);
   Serial.println("Wake up");
 }
 
-// обработчик аппаратного прерывания
-void isr_charge_up() { 
-  power.wakeUp();  
-  Serial.println("Sensor charging!");
+void batery_report() {
+  Serial.print("bat adc value:  ");
+  Serial.println(bat_adc);
 }
 
-void isr_charge_down() {
-  power.wakeUp();  
-  Serial.println("Sensor stop charging!");
-}
-
-void make_temp_string(char *arrayTemp, float tempC) {
+void make_temp_string(char *arrayTemp, short tempC) {
   String temp_string_maker = "";
   
   // Выставление + или - в зависимости от температуры
-  if (tempC > float(0)) {
+  if (tempC > 0) {
     temp_string_maker += '+';
-  } else if (tempC < float(0)) {
-    temp_string_maker += '-';
   }
 
-  temp_string_maker += String(int(tempC)) + "C";
+  temp_string_maker += String(tempC) + "C";
 
   strcpy(arrayTemp, temp_string_maker.c_str());
   
@@ -214,11 +143,51 @@ void make_temp_string(char *arrayTemp, float tempC) {
   Serial.println(arrayTemp);
 }
 
+void draw_temperature(short tempC) {
+  char arrayTemp[BUF_SIZE] = {0};
+  if (tempC < -100) { //ошибка измерений температуры
+    Serial.println("SENSOR ERROR");
+    char error[] = {'E', 'R', 'R'};
+    myGLCD.print(error, CENTER, BATERY_HEIGHT + 1);
+    delay(2000);
+    resetFunc();
+  } else {
+    make_temp_string(arrayTemp, tempC);
+    myGLCD.print(arrayTemp, CENTER, BATERY_HEIGHT + 1);
+  }
+}
+
 void draw_batery() {
-  if (digitalRead(BAT_CHRG_PIN) == LOW)
-    myGLCD.drawBitmap(BATERY_POSX, BATERY_POSY, ChargeBatery, BATERY_WIDTH, BATERY_HEIGHT);
-  else if (bat_adc == BAT_CHARGED_ADC)
-    myGLCD.drawBitmap(BATERY_POSX, BATERY_POSY, Batery, BATERY_WIDTH, BATERY_HEIGHT);
-  else
-    myGLCD.drawBitmap(BATERY_POSX, BATERY_POSY, LowBatery, BATERY_WIDTH, BATERY_HEIGHT);
+  Serial.print("Batery: ");
+  uint16_t batery_charge_range = BAT_CHARGED_ADC - BAT_DISCHARGED_ADC;
+  uint8_t *batery_pic;
+  if (is_charging()) {
+    Serial.println("100%");
+    batery_pic = ChargeBatery;
+  }
+  else if (bat_adc > 3/4 * batery_charge_range + BAT_DISCHARGED_ADC) {
+    Serial.println("100%");
+    batery_pic = Batery;
+  }
+  else if (bat_adc > 1/2 * batery_charge_range + BAT_DISCHARGED_ADC) {
+    Serial.println("75%");
+    batery_pic = Batery75;
+  }
+  else if (bat_adc > 1/4 * batery_charge_range + BAT_DISCHARGED_ADC) {
+    Serial.println("50%");
+    batery_pic = Batery50;
+  }
+  else if (bat_adc > BAT_DISCHARGED_ADC) {
+    Serial.println("25%");
+    batery_pic = Batery25;
+  }
+  else {
+    Serial.println("0%");
+    batery_pic = LowBatery;
+  }
+  myGLCD.drawBitmap(BATERY_POSX - BATERY_WIDTH, BATERY_POSY, batery_pic, BATERY_WIDTH, BATERY_HEIGHT);
+}
+
+bool is_charging() {
+  return digitalRead(BAT_CHRG_PIN) == LOW;
 }
